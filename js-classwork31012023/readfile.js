486ecb763a7a13f7a5f6fbfe5d80432767331625
Фёдоров Илья <dyudyafed006@gmail.com>
2023-09-23 17:50:16 +0600

    23 09 2023 restructurize

---
+++ js-classwork31012023/readfile.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+
+let text = fs.readFileSync('text.txt', 'utf8')
+
+console.log(text)
+fs.writeFileSync('text.txt, text' + '!')
+
+setInterval(function(){
+
+})
+
+// setInterval js sample
